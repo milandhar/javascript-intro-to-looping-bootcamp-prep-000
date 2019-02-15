50cfa29ada2602ca7c0ec65f9c5490b987f62b22
Milan Dhar <milan.dhar@gmail.com>
2019-02-15 18:17:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,7 @@
 function forLoop(array) {
   for(var i = 0; i < 25; i++) {
     if (i === 1) {
-      array = [...array, "I am 1 strange loop."]
-      return array
-      i++
+      array.push("I am 1 strange loop.")
     }
     else {
      array = [...array, `I am ${i} strange loops.`] 
